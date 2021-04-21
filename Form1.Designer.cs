157a6328f46009930313324d524952ba50996ab9
Даниил <korotkovdan@outlook.com>
2021-04-21 20:07:10 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Практика_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 180);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1037, 440);
+            this.chart1.TabIndex = 0;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(433, 65);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(433, 104);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(433, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Шаг";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(433, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Вывести температуры с";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(433, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "по";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "День",
+            "Неделя",
+            "Месяц",
+            "Год"});
+            this.comboBox1.Location = new System.Drawing.Point(433, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(200, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(433, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Обновить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1061, 632);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Задание №3. Погода в Москве.";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,101 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Xml.Linq;
+
+namespace Практика_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            /*Первоначальные настройки*/
+            chart1.Series.Clear();
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chart1.Series.Clear();
+            if (dateTimePicker2.Value <= dateTimePicker1.Value) { 
+                MessageBox.Show("Некорректно введена дата", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error); 
+                return; 
+            }
+            if ((comboBox1.SelectedIndex == 1 && dateTimePicker1.Value.AddDays(7) > dateTimePicker2.Value) ||
+               (comboBox1.SelectedIndex == 2 && dateTimePicker1.Value.AddMonths(1) > dateTimePicker2.Value) ||
+               (comboBox1.SelectedIndex == 3 && dateTimePicker1.Value.AddYears(1) > dateTimePicker2.Value))
+            {
+                MessageBox.Show("Выбран некорректный шаг", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }            
+                       
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    DayStep();
+                    break;
+                case 1:
+                    CustomStep(7);
+                    break;
+                case 2:
+                    CustomStep(31);
+                    break;
+                case 3:
+                    CustomStep(365);
+                    break;
+            }
+        }
+
+        private void DayStep()
+        {
+            Series s = new Series("Температура в Москве");
+            s.ChartType = SeriesChartType.Line;
+            var el = GetInfo_FromServer();
+            for (int i=0; i< el.Count(); i++)
+            {
+                s.Points.AddXY(DateTime.Parse(el[i].Element("date").Value), 
+                                Double.Parse(el[i].Element("temperature").Value.Replace('.',',')));
+            }
+            chart1.Series.Add(s);
+        }
+
+        private void CustomStep(int step)
+        {
+            Series s = new Series("Температура в Москве");
+            s.ChartType = SeriesChartType.Line;
+            var el = GetInfo_FromServer();
+            double sum = 0;
+            for (int i = 0; i < el.Count(); i++)
+            {
+                sum += Double.Parse(el[i].Element("temperature").Value.Replace('.', ','));
+                if (i%step == 0 && i != 0)
+                {
+                    s.Points.AddXY(DateTime.Parse(el[i].Element("date").Value), sum/step);
+                    sum = 0;
+                }                
+            }
+            chart1.Series.Add(s);
+        }
+
+        private List<XElement> GetInfo_FromServer()
+        {
+            String url = "https://api.meteostat.net/v1/history/daily?station=UUWW0&start=" + 
+                dateTimePicker1.Value.ToString("yyyy-MM-dd") + "&end=" + 
+                dateTimePicker2.Value.ToString("yyyy-MM-dd") + "&key=L23FNnUI";            
+            XDocument xDoc = JsonConvert.DeserializeXNode(new WebClient().DownloadString(url), "Root");              
+            return xDoc.Element("Root").Elements("data").ToList();
+        }
+
+    }
+}
